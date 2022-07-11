3253460d50f6ce26251f717fee7c7f21f11a0a70
Timemash <timemash24@gmail.com>
2022-07-11 17:50:15 +0900

    Upadte slide animation

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -1,37 +1,49 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import '../css/Transition.css';
 import AboutMe from '../routes/AboutMe';
 import Home from '../routes/Home';
 import Portfolio from '../routes/Portfolio';
-import '../css/Transition.css';
+
+let currentPath = '';
 
 function Transition() {
+  const [page, setPage] = useState('');
+
   const routes = [
     { path: '/', name: 'home', order: 1 },
     { path: '/portfolio', name: 'portfolio', order: 2 },
     { path: '/about_me', name: 'about_me', order: 3 },
   ];
 
-  const home = <Home />;
-  const portfolio = <Portfolio />;
-  const aboutMe = <AboutMe />;
+  let location = useLocation();
 
-  const location = useLocation();
+  // const getPage = (pathname) => {
+  //   routes.forEach((route) => {
+  //     if (pathname === route.path) {
+  //       setPage(route.name);
+  //     }
+  //   });
+  // };
+
+  // if (currentPath !== location) {
+  //   getPage(location.pathname);
+  // }
+  // currentPath = location;
 
   return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.pathname}
-        classNames="page-slider"
-        timeout={{ enter: 800, exit: 400 }}
-      >
-        <Routes>
-          <Route exact path="/" element={home} />
-          <Route path="/portfolio" element={portfolio} />
-          <Route path="/about_me" element={aboutMe} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
+    <div>
+      <TransitionGroup>
+        <CSSTransition key={location.pathname} classNames="slide" timeout={600}>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about_me" element={<AboutMe />} />
+          </Routes>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
   );
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,16 +3,18 @@ import styles from '../css/Home.module.css';
 
 function Home() {
   return (
-    <div className={styles.home}>
-      <h1 className={styles.title}>HOME</h1>
-      <ul className={styles.menu}>
-        <li>
-          <Link to="/portfolio">PORTFOLIO</Link>
-        </li>
-        <li>
-          <Link to="/about_me">ABOUT ME</Link>
-        </li>
-      </ul>
+    <div className={styles.background}>
+      <section className={styles.home}>
+        <h1 className={styles.title}>HOME</h1>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/portfolio">PORTFOLIO</Link>
+          </li>
+          <li>
+            <Link to="/about_me">ABOUT ME</Link>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 }
