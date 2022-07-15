fa1a16ffffe525f9bd4b3f1b153b0a2ea98f0c79
Timemash <timemash24@gmail.com>
2022-07-16 02:04:41 +0900

    Update css styles and navbar actions

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,73 @@
-import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import styles from '../css/Nav.module.css';
+
+let currentPath = '';
 
 function Nav() {
+  let location = useLocation();
+
+  const routes = [
+    { pathname: '/', name: 'HOME' },
+    { pathname: '/portfolio', name: 'PORTFOLIO' },
+    { pathname: '/about_me', name: 'ABOUT ME' },
+  ];
+
+  const transition = {
+    duration: 0.7,
+    ease: [0.43, 0.13, 0.23, 0.96],
+  };
+  const variants = {
+    exit: { y: '-10%', opacity: 0, transition },
+    enter: {
+      y: '0%',
+      opacity: 1,
+      transition,
+    },
+  };
+
+  const [visible, setVisible] = useState(false);
+  const [curPage, setCurPage] = useState([]);
+
+  const onMouseOverNav = () => {
+    setVisible(!visible);
+  };
+
+  const onMouseOverMenu = () => {
+    setVisible(true);
+  };
+  const onMouseOut = () => {
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    setCurPage(location.pathname);
+  });
+
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">HOME</Link>
-        </li>
-        <li>
-          <Link to="/portfolio">PORTFOLIO</Link>
-        </li>
-        <li>
-          <Link to="/about_me">ABOUT ME</Link>
-        </li>
-      </ul>
+    <div className={styles.navbar}>
+      <button onMouseOver={onMouseOverNav}>Nav</button>
+      {visible ? (
+        <motion.ul key="nav" variants={variants} initial="exit" animate="enter">
+          <div>
+            {routes.map((route) => {
+              if (route.pathname !== curPage) {
+                return (
+                  <Link key={route.name.toLowerCase()} to={route.pathname}>
+                    <motion.li
+                      onMouseOver={onMouseOverMenu}
+                      onMouseOut={onMouseOut}
+                    >
+                      {route.name}
+                    </motion.li>
+                  </Link>
+                );
+              }
+            })}
+          </div>
+        </motion.ul>
+      ) : null}
     </div>
   );
 }
--- src/routes/AboutMe.js
+++ src/routes/AboutMe.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Nav from '../components/Nav';
 import styles from '../css/AboutMe.module.css';
 
