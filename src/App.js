b680da8fcfb1a4520bd35d33f6ce5cc0c09928ae
Timemash <timemash24@gmail.com>
2022-07-17 01:43:41 +0900

    Change to HashRouter for 404 issue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Transition from './components/Transition';
 
 function App() {
