2b28078c42d934269f6b633db0c7000c33a96b6a
Timemash <timemash24@gmail.com>
2022-07-16 23:08:30 +0900

    Update portfolio page to show multiple projects Update css styles and animations

--- src/components/MyProject.js
+++ src/components/MyProject.js
@@ -1,9 +1,54 @@
-function MyProject() {
+import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
+import styles from '../css/Portfolio.module.css';
+
+function MyProject({ img, link, title, tags, desc, order }) {
+  const transition = {
+    duration: 0.7,
+    ease: [0.43, 0.13, 0.23, 0.96],
+    delay: 0.25 * order,
+  };
+
+  const imageVariants = {
+    exit: { y: '10%', opacity: 0, transition },
+    enter: {
+      y: '0%',
+      opacity: 1,
+      transition,
+    },
+  };
+
   return (
-    <div>
-      <p>my project</p>
-    </div>
+    <motion.section
+      key={title}
+      className={styles.project}
+      variants={imageVariants}
+      initial="exit"
+      animate="enter"
+      exit="exit"
+    >
+      <a href={link}>
+        <img src={img} alt={title} />
+      </a>
+      <div className={styles.project_info}>
+        <h3>{title}</h3>
+        <ul>
+          {tags.map((tag) => {
+            return <li key={tag}># {tag}</li>;
+          })}
+        </ul>
+        <p>{desc}</p>
+      </div>
+    </motion.section>
   );
 }
+
+MyProject.propTypes = {
+  img: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+  desc: PropTypes.string.isRequired,
+};
 
 export default MyProject;
--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -4,6 +4,31 @@ import Nav from '../components/Nav';
 import styles from '../css/Portfolio.module.css';
 
 function Portfolio() {
+  const root = 'https://timemash24.github.io/';
+  const projects = [
+    {
+      img: require('../img/img_momentum.png'),
+      link: `${root}momentumJS/`,
+      title: 'Personal Dashboard App',
+      tags: ['javascript', 'html', 'css'],
+      desc: 'Featuring inspiring photo, quote, weather and to-do list',
+    },
+    {
+      img: require('../img/img_paint.png'),
+      link: `${root}paintJS/`,
+      title: 'Paint App',
+      tags: ['javascript', 'html', 'css'],
+      desc: 'Draw, paint, save',
+    },
+    {
+      img: require('../img/img_movie.png'),
+      link: `${root}movie_app_react/`,
+      title: 'Movie App',
+      tags: ['react', 'javascript', 'html', 'css'],
+      desc: 'Introduce movies and sort by genres',
+    },
+  ];
+
   const transition = {
     duration: 0.7,
     ease: [0.43, 0.13, 0.23, 0.96],
@@ -19,37 +44,44 @@ function Portfolio() {
   };
 
   const textVariants = {
-    exit: { x: 100, opacity: 0, transition },
+    exit: { x: -100, opacity: 0, transition },
     enter: { x: 0, opacity: 1, transition: { delay: 0.3, ...transition } },
   };
 
   return (
     <motion.div
       key="portfolio"
-      className={styles.my_projects}
+      className={styles.portfolio}
       initial={{ opacity: 0.5 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0.5 }}
     >
       <Nav />
-      <section>
+      <div className={styles.portfolio_main}>
         <motion.h1
           variants={textVariants}
           initial="exit"
           animate="enter"
           exit="exit"
         >
-          PORTFOLIO
+          MY TOY PROJECTS
         </motion.h1>
-        <motion.section
-          variants={imageVariants}
-          initial="exit"
-          animate="enter"
-          exit="exit"
-        >
-          <MyProject />
-        </motion.section>
-      </section>
+        <motion.div className={styles.projects}>
+          {projects.map((project, i) => {
+            return (
+              <MyProject
+                key={i}
+                img={project.img}
+                link={project.link}
+                title={project.title}
+                tags={project.tags}
+                desc={project.desc}
+                order={i}
+              />
+            );
+          })}
+        </motion.div>
+      </div>
     </motion.div>
   );
 }
