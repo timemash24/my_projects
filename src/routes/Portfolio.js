b194362b4cffe3fce1c08a766dfbae971645c191
Timemash <timemash24@gmail.com>
2022-08-10 21:48:13 +0900

    Update portfolio page with adding scroll index

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion';
 import MyProject from '../components/MyProject';
 import Nav from '../components/Nav';
 import styles from '../css/Portfolio.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClover } from '@fortawesome/free-solid-svg-icons';
 
 function Portfolio() {
   const root = 'https://timemash24.github.io/';
@@ -33,6 +35,12 @@ function Portfolio() {
   const scrollRef = useRef();
   const [deltaY, setDeltaY] = useState(0);
   const [curTop, setCurTop] = useState(0);
+  const [curIndex, setCurIndex] = useState(0);
+
+  // 스크롤 방향 확인
+  const yWheelEvent = (e) => {
+    setDeltaY(e.deltaY);
+  };
 
   useEffect(() => {
     if (!scrollRef.current) return;
@@ -46,28 +54,27 @@ function Portfolio() {
     const scrollHeight = e.target.scrollHeight; // 전체 스크롤 높이
     const scrollTop = e.target.scrollTop; // 현재 스크롤 위치
 
-    // 위로 스크롤
+    // 아래로 스크롤
     if (deltaY > 0) {
       if (curTop < scrollHeight / 3) {
         e.target.childNodes[1].scrollIntoView({ block: 'center' });
-      } else if (curTop < (scrollHeight / 3) * 2) {
+        setCurIndex(1);
+      } else {
         e.target.childNodes[2].scrollIntoView();
+        setCurIndex(2);
       }
     }
-    // 아래로 스크롤
+    // 위로 스크롤
     else {
-      if (curTop >= scrollHeight / 3) {
+      if (curTop > scrollHeight / 3) {
         e.target.childNodes[1].scrollIntoView();
-      } else if (curTop < scrollHeight / 3 - 150) {
+        setCurIndex(1);
+      } else if (curTop <= scrollHeight / 4) {
         e.target.childNodes[0].scrollIntoView({ block: 'center' });
+        setCurIndex(0);
       }
     }
     setCurTop(scrollTop);
-  };
-
-  // 스크롤 방향 확인
-  const yWheelEvent = (e) => {
-    setDeltaY(e.deltaY);
   };
 
   return (
@@ -79,6 +86,7 @@ function Portfolio() {
       exit={{ opacity: 0.5 }}
     >
       <Nav />
+
       <div className={styles.projects} onScroll={onScroll} ref={scrollRef}>
         {projects.map((project, i) => {
           return (
@@ -95,6 +103,22 @@ function Portfolio() {
           );
         })}
       </div>
+      <section className={styles.projects_index}>
+        {projects.map((_, i) => {
+          return (
+            <span
+              className={
+                i === curIndex
+                  ? `${styles.current_index}`
+                  : `${styles.other_index}`
+              }
+              key={`pIndex${i}`}
+            >
+              <FontAwesomeIcon icon={faClover} />
+            </span>
+          );
+        })}
+      </section>
     </motion.div>
   );
 }
